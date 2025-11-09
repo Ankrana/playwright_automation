2e24c2060dc84baecbffd09ce16753672b27a131
Ankit Rana <ankitrana@Ankits-MacBook-Pro.local>
2025-11-09 12:16:03 -0500

    Initial Playwright Setup

---
+++ tests/googleTitle.spec.js
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test("Verify page title", async ({page}) => {
+
+await page.goto('https://www.google.com');
+await expect (page).toHaveTitle(/Google/);
+
+})
